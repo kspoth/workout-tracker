3bbe8876123f5aa3251b1f7aff3624a22bd23ce5
Kevin Spoth <kspoth08@su.edu>
2021-03-30 13:44:16 -0400

    adding routes

---
+++ routes/api.js
@@ -0,0 +1,54 @@
+const router = require("express").Router();
+const { query } = require("express");
+const db = require("../models");
+
+//set Api get route
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+//set Api get route for date range
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+//set Api post route
+router.post("/api/workouts", (req, res) => {
+  console.log("hit the post");
+  db.Workout.create({})
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+//set Api put route
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  db.Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
